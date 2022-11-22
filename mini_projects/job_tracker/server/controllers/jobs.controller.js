e164b14c8167fa675b150be89f657d57f53ff88d
Melissa <MelissaCode01@outlook.com>
2022-11-22 17:47:01 -0500

    Update jobs.controller.js
    Updates to server controller and routing.

--- mini_projects/job_tracker/server/controllers/jobs.controller.js
+++ mini_projects/job_tracker/server/controllers/jobs.controller.js
@@ -7,16 +7,25 @@ module.exports.test = (req, res) => {
 
 // get all
 module.exports.allJobs = (req, res) => {
+    Job.find()
+        .then(jobs => res.json(jobs))
+        .catch(err => res.status(400).json)
 
 }
 
 // get one
 module.exports.oneJob = (req, res) => {
+    Job.findOne({_id: req.params.id})
+        .then(job => res.josn(job))
+        .catch(err => res.status(400).json)
 
 }
 
 // create
 module.exports.createJob = (req, res) => {
+    Job.create(req.body)
+        .then(newJob => res.json(newJob))
+        .catch(err =>res.status(400).json)
 
 }
 
