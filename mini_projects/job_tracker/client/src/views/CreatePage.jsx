eb7211c184eabce8bfd59b8c9ca1f81944f9ba34
Melissa <MelissaCode01@outlook.com>
2022-11-28 18:53:26 -0500

    job_tracker_app
    Client-side: Add Job form css update

--- mini_projects/job_tracker/client/src/views/CreatePage.jsx
+++ mini_projects/job_tracker/client/src/views/CreatePage.jsx
@@ -21,55 +21,68 @@ const CreatePage = () => {
     }
 
     return (
-        <div className="add_job_form">
-            <form onSubmit={handleSubmit}  >
-                <div>
-                    <label className="col-20">Company: </label>
-                    <div className="col-80">
-                        <input type="text" id="company" name="company" placeholder="Company" value={company}  onChange={(e)=>setCompany(e.target.value)}></input>
+            
+            <div className='add_job_container'>
+                <form onSubmit={handleSubmit}>
+
+                    <div className='row'>
+                        <div className='col-15'>
+                            <label className="col-15">Company: </label>
+                        </div>
+                        <div className='col-85'>
+                            <input type="text" id="company" name="company" placeholder="Company" value={company}  onChange={(e)=>setCompany(e.target.value)}></input>
+                        </div>
                     </div>
-                </div>
 
-                <div>
-                    <label className="col-20">Position Title: </label>
-                    <div className="col-80">
-                        <input type="text" id="title" name="title" placeholder="Enter Title"  value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+
+                    <div className='row'>
+                        <div className='col-15'>
+                            <label className="col-20">Position Title: </label>
+                        </div>
+                        <div className='col-85'>
+                            <input type="text" id="title" name="title" placeholder="Enter Title"  value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+                        </div>
                     </div>
-                </div>
 
-                <div>
-                    <label className="col-20">Salary: </label>
-                    <div className="col-80">
-                        <input type="number" id="salary" name="salary" placeholder="Salary" value={salary}  onchange={(e)=>setSalary(e.target.value)}></input>               
+
+                    <div className='row'>
+                        <div className='col-15'>
+                            <label className="col-20">Salary: </label>
+                        </div>
+                        <div className='col-85'>
+                            <input type="number" id="salary" name="salary" placeholder="Salary" value={salary}  onchange={(e)=>setSalary(e.target.value)}></input>               
+                        </div>
                     </div>
-                </div> 
 
-                <div>
-                    <div className="col-80">
-                        <input type="checkbox" id="isRemote" name="isRemote" checked={isRemote} onChange={(e)=>setIsRemote(e.target.checked)}></input>
+
+                    <div className='row'>
+                        <div className='col-15'>
+                            <label className="col-20">Remote?</label> 
+                        </div>
+                        <div className='col-85'>
+                            <input type="checkbox" id="isRemote" name="isRemote" checked={isRemote} onChange={(e)=>setIsRemote(e.target.checked)}></input>
+                        </div>
                     </div>
-                    <label className="col-20">Remote?</label>
-                </div>
 
-                <div>
-                    <label htmlFor="col-20">Additional Notes: </label>
-                    <div className="col-80">
-                        <input type="textarea" id="textarea" name="textarea" placeholder="Any additional notes?" value={notes} ></input>
+
+                    <div className='row'>
+                        <div className='col-15'>
+                            <label className="col-80">Additional Notes: </label>                            
+                        </div>
+                        <div className='col-85'>
+                            <input type="textarea" id="textarea" name="textarea" placeholder="Any additional notes?" value={notes}  onChange={(e)=>setNotes(e.target.value)}></input>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <button type="submit" id="create_button">Add Job</button> 
-                </div>
+
+                    <div className='row'>
+                        <input type="submit" value="Add Job"></input>
+                        <button type="submit" id="add_job_button">Add Job</button> 
+                    </div>
+                    
 
 
-
-                
-
-                
-            </form>
-
-
-        </div>
+                </form>
+            </div>
     )
 }
 
